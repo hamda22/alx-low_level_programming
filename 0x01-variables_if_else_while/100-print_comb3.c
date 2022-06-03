611ffd6a012412a103e6d4466564f96f61ce7277
hamda22 <xamdacilmi@gmail.com>
2022-06-03 15:54:09 -0700

    task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,40 +1,33 @@
 #include <stdio.h>
 
 /**
- * main - print triple combos
+ * main - print double digit combos
  *
- * Description: print triple digit combos
+ * Description: print double digit combos
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int i, j, k;
+	int i, j;
 
 	i = 48;
 	j = 48;
-	k = 48;
 
 	while (i < 58)
 	{
 		j = i + 1;
 		while (j < 58)
 		{
-			k = j + 1;
-			while (k < 58)
-			{
-				putchar(i);
-				putchar(j);
-				putchar(k);
-				if (i < 55 || j < 56 || k < 57)
-				{
-					putchar(44);
-					putchar(32);
-				}
-				k++;
-			}
+			putchar(i);
+			putchar(j);
 
+			if (i < 56 || j < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
 			j++;
 		}
 
